1d697ffd4f640d30366c823d0921f9b0c75af343
Chris <93446223+Christoph2209@users.noreply.github.com>
2025-04-25 10:27:16 -0400

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,3 +1,3 @@
-save_dir                    = './tinyImageNet_images/'
+save_dir = './tinyImageNet_images/'
 data_dir = {}
-data_dir['CUB']             = './tinyImageNet_images/'
+data_dir['CUB'] = './tinyImageNet_images/'
